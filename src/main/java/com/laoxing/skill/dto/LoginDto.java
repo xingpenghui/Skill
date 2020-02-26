8c1e9d70992f08b92bcae91b30937bc4f3a597d8
feri <xingfei_work@163.com>
2020-02-26 12:00:50 +0800

    唯一登录的代码实现，LoginDto

---
+++ src/main/java/com/laoxing/skill/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package com.laoxing.skill.dto;
+
+import lombok.Data;
+
+/**
+ * @program: Skill
+ * @description:
+ * @author: Feri
+ * @create: 2020-02-26 10:09
+ */
+@Data
+public class LoginDto {
+    private String phone;
+    private String pwd;
+}
